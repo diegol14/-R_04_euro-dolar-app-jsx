edcc2e0f2606fd6f45625838d3835b37f4b4f7af
diegol14 <diegolpereira1414@gmail.com>
2024-09-12 20:41:40 +0200

    firstOne

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const title = <h1 className='titleApp'>Conversor Euros-Dólares</h1>
+  const eurosRef = useRef();
+  const showValueRef = useRef();
+
+  const convertirEuros=()=>{
+ 
+    const eurosVal = eurosRef.current.value ;
+    const dollars = eurosVal * 1.08;
+    showValueRef.current.innerHTML= dollars.toFixed(2) + " $";
+ 
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div>{title}</div>
+      <input type="text" ref={eurosRef} placeholder="Cantidad en Euros" />
+      <div>
+        <div className='App'>
+          <button onClick={convertirEuros}>Convert!!</button>
+        </div>
+        <div ref={showValueRef} > 
+          </div>
+      </div>
+      <div>
+        <p></p>
+      </div>
     </div>
   );
 }
